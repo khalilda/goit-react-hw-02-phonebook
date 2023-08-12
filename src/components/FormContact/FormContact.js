2a3d4e15f60c43af28c934006d74d19d5e5bf318
khalilda <100872154+khalilda@users.noreply.github.com>
2023-08-12 20:26:33 +0300

    homework react 2

--- src/components/FormContact/FormContact.js
+++ src/components/FormContact/FormContact.js
@@ -35,7 +35,7 @@ export class FormContact extends Component {
               type="text"
               name="name"
               placeholder="Name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               value={this.state.name}
@@ -48,7 +48,7 @@ export class FormContact extends Component {
               type="tel"
               name="number"
               placeholder="Number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               value={this.state.number}
